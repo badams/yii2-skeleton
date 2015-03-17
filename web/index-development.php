3b3690ee51dac174d222be4ef051f75f1928679a
Byron Adams <byron@webtools.co.nz>
2015-03-17 20:07:12 +1300

    Adding index-development.php

---
+++ web/index-development.php
@@ -0,0 +1,19 @@
+<?php
+
+// Env Types
+define('ENV_DEVELOPMENT', 0);
+define('ENV_TESTING', 1);
+define('ENV_STAGING', 2);
+define('ENV_PRODUCTION', 3);
+
+define('YII_ENV', ENV_PRODUCTION);
+
+
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+$config = require(__DIR__ . '/../config/web.php');
+
+(new yii\web\Application($config))->run();
